d3c879f20a4a61f81cd6edc84f585ffc67200bbe
Martijn Goorden <m.a.goorden@tue.nl>
2021-07-01 16:30:07 +0200

    Add python script that combines swmm and uppaal

--- SWMMandStratego/simplemodeltest.py
+++ SWMMandStratego/simplemodeltest.py
@@ -1,7 +1,73 @@
-import csv
-import datetime
+import sys
+from functools import cache
+
+import pyswmm
 import strategoutil as sutil
+from pyswmm import Simulation, Nodes, Links
+import os.path
+import re
 import yaml
+import datetime
+import csv
+
+
+def insert_rain_data_file_path(swmm_inputfile, rain_data_file):
+    """
+    Insert the provided rain data file path into the swmm model.
+
+    :param str swmm_inputfile: swmm model path
+    :param str rain_data_file: rain data file path
+    """
+    with open(swmm_inputfile, "r+") as f:
+        file_content = f.read()
+        new_line = "long_term_rainfallgauge5061 FILE \"" + rain_data_file + "\""
+        file_content = re.sub(r"long_term_rainfallgauge5061 FILE \"[^\"]*\"", new_line,
+                              file_content, count=1)
+        f.seek(0)
+        f.write(file_content)
+        f.truncate()
+
+
+def run_swmm(swmm_inputfile, basin_id, orfice_id, time_step, output_csv_file, water_level,
+             valve_opening, start_time, end_time):
+    """
+    Extracts the time sequential water level in the specified basin from swmm model and write it to
+    a csv file.
+
+    :param str swmm_inputfile: swmm model path
+    :param str basin_id: basin id from swmm
+    :param int time_step: time interval in seconds
+    :param str csv_file_basename: csv file basename
+    """
+    time_series = []
+    water_depth = []
+
+    with Simulation(swmm_inputfile) as sim:
+        su = Nodes(sim)[basin_id]
+        su.initial_depth = water_level
+        orf = Links(sim)[orfice_id]
+        orf.target_setting = valve_opening
+
+        sim.start_time = start_time
+        sim.end_time = end_time
+
+        # TODO: Figure out what the difference is between the above options and using init_conditions
+        # def init_conditions():
+        #     su.initial_depth = water_level
+        # sim.initial_conditions(init_conditions)
+
+        sim.step_advance(time_step)
+        for step in sim:
+            time_series.append(sim.current_time)
+            water_depth.append(su.depth)
+
+    sys.stdout.flush()
+    with open(output_csv_file, "w") as f:
+        writer = csv.writer(f)
+        for i, j in zip(time_series, water_depth):
+            writer.writerow([i, j])
+
+    return water_depth[len(water_depth) - 1]
 
 
 def create_weather_forecast(rain_data_file, weather_forecast_file, start_date, horizon,
@@ -9,46 +75,26 @@ def create_weather_forecast(rain_data_file, weather_forecast_file, start_date, h
     """
     Create a weather forecast from historical rain data.
     """
+    # TODO: add caching
     rain_data = read_rain_data(rain_data_file)
     weather_forecast = calculate_weather_intervals(rain_data, start_date, horizon, uncertainty)
     write_weather_forecast(weather_forecast_file, weather_forecast)
 
 
+@cache
 def read_rain_data(data_file):
     """
     Read the historical rain data from file.
     """
-    with open(data_file, "r") as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
+    with open(data_file, "r") as f:
+        lines = f.readlines()
         rain_data = []
-        for row in csv_reader:
-            dt = datenum_to_datetime(float(row[0]))
-            dt = round_datetime_to_minute(dt)
-            rain_data.append([dt, float(row[1])])
+        for line in lines:
+            words = line.split()
+            # In <name>.dat rain file, we have mm/dd/yyyy hh:mm:ss x.y per line.
+            dt = datetime.datetime.strptime(words[0] + " " + words[1], "%m/%d/%Y %H:%M:%S")
+            rain_data.append([dt, float(words[2]) / 60])
     return rain_data
-
-
-def datenum_to_datetime(datenum):
-    """
-    Convert Matlab datenum into Python datetime.
-
-    Notes on day counting
-    matlab: day one is 1 Jan 0000
-    python: day one is 1 Jan 0001
-    Hence a reduction of 366 days, for year 0 AD was a leap year
-    """
-    days = datenum % 1
-    return datetime.datetime.fromordinal(int(datenum) - 366) + datetime.timedelta(days=days)
-
-
-def round_datetime_to_minute(dt):
-    """
-    Rounds a datetime object to its nearest minute.
-    """
-    dt += datetime.timedelta(seconds=30)
-    dt -= datetime.timedelta(seconds=dt.second,
-                             microseconds=dt.microsecond)
-    return dt
 
 
 def calculate_weather_intervals(rain_data, start_date, horizon, uncertainty):
@@ -157,7 +203,7 @@ class MPCSetupPond(sutil.SafeMPCSetup):
             f.write(line1.format(horizon, period, final))
             f.write("\n")
             line2 = "simulate 1 [<={}+1] {{ {} }} under opt\n"
-            f.write(line2.format(period, self.controller.print_var_names()))
+            f.write(line2.format(period, self.controller.get_var_names_as_string()))
 
     def create_alternative_query_file(self, horizon, period, final):
         """
@@ -171,7 +217,7 @@ class MPCSetupPond(sutil.SafeMPCSetup):
             f.write(line1.format(horizon, period, final))
             f.write("\n")
             line2 = "simulate 1 [<={}+1] {{ {} }} under opt\n"
-            f.write(line2.format(period, self.controller.print_var_names()))
+            f.write(line2.format(period, self.controller.get_var_names_as_string()))
 
     def perform_at_start_iteration(self, controlperiod, horizon, duration, step, **kwargs):
         """
@@ -184,17 +230,64 @@ class MPCSetupPond(sutil.SafeMPCSetup):
                                 kwargs["weather_forecast_path"], current_date,
                                 horizon * controlperiod, kwargs["uncertainty"])
 
+    def run_external_simulator(self, chosen_action, controlperiod, step, **kwargs):
+        max_out_flow = 60.0 * 95000.0  # Max outflow [cm3/min], 95000 cm3/s = 95 l/s.
+        start_datetime = kwargs["start_date"] + datetime.timedelta(minutes=step * controlperiod)
+        end_datetime = kwargs["start_date"] + datetime.timedelta(minutes=(step + 1) * controlperiod + 1)
+
+        # Run the SWMM model.
+        new_water_level = run_swmm(swmm_inputfile,
+                                   kwargs["basin_id"],
+                                   kwargs["orfice_id"],
+                                   kwargs["swmm_time_step"],
+                                   kwargs["swmm_result_file"],
+                                   self.controller.get_state("w") / 100,  # Unit controller = cm, unit SWMM = m.
+                                   chosen_action / max_out_flow,
+                                   start_datetime,
+                                   end_datetime)
+
+        # Get the new state.
+        return {"w": new_water_level * 100, "t": self.controller.get_state("t") + controlperiod}
+
 
 if __name__ == "__main__":
-    # Define location of the relevant files and commands.
-    model_template_path = "../uppaal/pond_ADHS_external_weather_online.xml"
-    query_file_path = "../uppaal/pond_ADHS_query.q"
-    model_config_path = "../uppaal/model_config.yaml"
-    learning_config_path = "../uppaal/verifyta_config.yaml"
-    historical_rain_data_path = "../uppaal/Rain_Ts.txt"
-    weather_forecast_path = "../uppaal/weather_forecast.csv"
-    output_file_path = "../uppaal/result.txt"
+    # Assumed folder structure:
+    # CLAIRE
+    # - scripts
+    # - swmm_models
+    # - uppaal
+    # - SWMMandStratego <- This file should be in this folder. TODO: maybe move it to scripts.
+
+    # First figure out where the swmm model file is located. This is also OS dependent.
+    this_file = os.path.realpath(__file__)
+    base_folder = os.path.dirname(os.path.dirname(this_file))
+    swmm_folder = "swmm_models"
+    swmm_inputfile = os.path.join(base_folder, swmm_folder, "test4_swmm_simulation_control.inp")
+    assert (os.path.isfile(swmm_inputfile))
+    swmm_result_file = os.path.join(base_folder, swmm_folder, "swmm_results.csv")
+
+    # We found the model. Now we have to include the correct path to the rain data into the model.
+    rain_data_file = "swmm_5061.dat"  # Assumed to be in the same folder as the swmm model input file.
+    rain_data_file = os.path.join(base_folder, swmm_folder, rain_data_file)
+    insert_rain_data_file_path(swmm_inputfile, rain_data_file)
+
+    # Now we locate the Uppaal folder and files.
+    uppaal_folder_name = "uppaal"
+    uppaal_folder = os.path.join(base_folder, uppaal_folder_name)
+    model_template_path = os.path.join(uppaal_folder, "pond_ADHS_external_weather_online.xml")
+    query_file_path = os.path.join(uppaal_folder, "pond_ADHS_query.q")
+    model_config_path = os.path.join(uppaal_folder, "model_config.yaml")
+    learning_config_path = os.path.join(uppaal_folder, "verifyta_config.yaml")
+    weather_forecast_path = os.path.join(uppaal_folder, "weather_forecast.csv")
+    output_file_path = os.path.join(uppaal_folder, "result.txt")
     verifyta_command = "verifyta-stratego-8-7"
+
+    # Define model variables.
+    external_simulator = True
+    action_variable = "Qout"
+    basin_id = "SU1"
+    orfice_id = "OR1"
+    swmm_time_step = 60  # Unit is seconds.
 
     # Whether to run in debug mode.
     debug = True
@@ -209,7 +302,9 @@ if __name__ == "__main__":
     controller = MPCSetupPond(model_template_path, output_file_path, queryfile=query_file_path,
                               model_cfg_dict=model_cfg_dict,
                               learning_args=learning_cfg_dict,
-                              verifytacommand=verifyta_command, debug=debug)
+                              verifyta_command=verifyta_command,
+                              external_simulator=external_simulator,
+                              action_variable=action_variable, debug=debug)
 
     # Define the MPC parameters.
     period = 60  # Period in time units (minutes).
@@ -219,5 +314,7 @@ if __name__ == "__main__":
     uncertainty = 0.1  # The uncertainty in the weather forecast generation.
 
     controller.run(period, horizon, duration, start_date=start_date,
-                   historical_rain_data_path=historical_rain_data_path,
-                   weather_forecast_path=weather_forecast_path, uncertainty=uncertainty)
+                   historical_rain_data_path=rain_data_file,
+                   weather_forecast_path=weather_forecast_path, uncertainty=uncertainty,
+                   basin_id=basin_id, orfice_id=orfice_id, swmm_time_step=swmm_time_step,
+                   swmm_result_file=swmm_result_file)
